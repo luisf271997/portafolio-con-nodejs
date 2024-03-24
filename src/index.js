47ab09204db53fcc26e939e213d2011b33da8931
luisf271997 <127575865+luisf271997@users.noreply.github.com>
2024-03-24 17:08:03 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,27 @@
+//importacion de modulos
+import express from "express";
+import {dirname, join} from  'path';
+import {fileURLToPath} from 'url';
+import indexRoutes from "./routes/index.js"
+//configuracion de express y escucha del puerto
+const app =  express()
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const port = 3000
+/*console.log(join(__dirname,'views'))*/
+
+
+app.set( 'views', join(__dirname, "views"))
+
+app.set('view engine', 'ejs')
+
+app.use(indexRoutes)
+app.use(express.static(join(__dirname, 'public')))
+
+app.get("/",(req,res)=>res.render('index'))
+app.get("/about",(req,res)=>res.render('about'))
+app.get("/contact",(req,res)=>res.render('contact'))
+app.get("/projects",(req,res)=>res.render('projects'))
+
+app.listen(process.env.PORT || port)
+console.log("el servidpor esta activo en el puerto", 3000);
